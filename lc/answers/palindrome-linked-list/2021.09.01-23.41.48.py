c806d4b6f777cb72935c1da75e6f987004a37c3c
jsphweid <jsphweid@gmail.com>
2021-09-01 23:56:00 -0500

    palindrome-linked-list

---
+++ lc/answers/palindrome-linked-list/2021.09.01-23.41.48.py
@@ -0,0 +1,14 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def isPalindrome(self, head: Optional[ListNode]) -> bool:
+        if not head:
+            return True
+        data = []
+        while head:
+            data.append(head.val)
+            head = head.next
+        return data == list(reversed(data))
