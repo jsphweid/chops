2561632ba0dcad52bec804609665927fe904237c
jsphweid <jsphweid@gmail.com>
2021-09-05 14:57:49 -0500

    range-sum-query-immutable

---
+++ lc/answers/range-sum-query-immutable/2021.09.05-14.37.17.py
@@ -0,0 +1,12 @@
+class NumArray:
+
+    def __init__(self, nums: List[int]):
+        self._nums = nums
+
+    def sumRange(self, left: int, right: int) -> int:
+        return sum(self._nums[left:right + 1])
+
+
+# Your NumArray object will be instantiated and called as such:
+# obj = NumArray(nums)
+# param_1 = obj.sumRange(left,right)
