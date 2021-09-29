8fb6209743bd1e6bc5f08fff6af3f3e81a9963d8
jsphweid <jsphweid@gmail.com>
2021-09-29 18:47:00 -0500

    invert-binary-tree

---
+++ lc/answers/invert-binary-tree/2021.09.29-18.44.02.py
@@ -0,0 +1,10 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def invertTree(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
+        if not root: return None
+        return TreeNode(root.val, left=self.invertTree(root.right), right=self.invertTree(root.left))
