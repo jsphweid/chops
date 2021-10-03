147f0c0fe9a089253dd16592177e9cf0f7abe913
jsphweid <jsphweid@gmail.com>
2021-10-03 12:21:12 -0500

    can-place-flowers

--- lc/answers/can-place-flowers/2021.10.03-11.54.43.py
+++ lc/answers/can-place-flowers/2021.10.03-11.54.43.py
@@ -1,8 +1,8 @@
 class Solution:
     def canPlaceFlowers(self, flowerbed: List[int], n: int) -> bool:
         total = 0
-        streak = 0
-        for num in [0] + flowerbed + [0]:
+        streak = 1
+        for num in flowerbed:
             if num == 0:
                 streak += 1
             else:
@@ -11,4 +11,4 @@ class Solution:
             if streak == 3:
                 streak = 1
                 total += 1
-        return total >= n
+        return (total + (streak + 1 == 3)) >= n
