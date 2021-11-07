df898930329beff03b46255a4d871ecc319475c6
jsphweid <jsphweid@gmail.com>
2021-11-06 22:09:31 -0500

    another solution

--- lc/answers/count-and-say/2021.11.06-21.26.43.py
+++ lc/answers/count-and-say/2021.11.06-21.26.43.py
@@ -23,14 +23,42 @@ count up using their logic
 Time - 
 Space - 
 """
-import re
+"""
+char=1 prev="1", count=1
+char=2 prev="2", count=1
+char=1 prev="1", count=1
+char=1 prev="1", count=2
+"""
+def count_chars(s: str):  # "1211" -> [(1, '1'), (1, '2'), (2, '1')]
+    groups = []
+    count = 0
+    previous_char = None
+    for char in s:
+        if previous_char:
+            if previous_char != char:
+                groups.append((count, previous_char))
+                count = 1
+                previous_char = char
+            else:
+                count += 1
+        else:
+            previous_char = char
+            count = 1
+    if count:
+        groups.append((count, previous_char))
+    return groups
+
+# 1211 ([(1, '1'), (1, '2'), (2, '1')]) -> 111221 
+def compute_next(curr: str):
+    s = ""
+    for count, char in count_chars(curr):
+        s += str(count) + char
+    return s
+
 class Solution:
     def countAndSay(self, n: int) -> str:
         if n == 1: return "1"
         base = "1"
         for _ in range(n - 1):
-            new_str = ""
-            for group, char in re.findall(r"((.)\2*)", base):
-                new_str += str(len(group)) + char
-            base = new_str
+            base = compute_next(base)
         return base
