fa2994e3624447f78a14a688c0737c7412a2aff9
jsphweid <jsphweid@gmail.com>
2021-11-13 10:41:18 -0600

    combination-sum

---
+++ lc/answers/combination-sum/2021.11.13-10.30.53.py
@@ -0,0 +1,27 @@
+"""
+recurse([2], 2)
+    recurse([2,3,7], [2,2], 4)
+        recurse([2,3,7], [2,2,2], 6) -> None (eventually)
+        recurse([3,7], [2,2,3], 7) -> ANSWER
+        recurse([7], [2,2,7], 11) -> None
+    recurse([3,7], [2,3], 5)
+        recurse([3,7], [2,3], 5)
+    recurse([7], [2,7], 9) -> None
+recurse([3], 3)
+recurse([7], 7) -> ANSWER
+"""
+
+class Solution:
+    def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
+        res = []
+        def recurse(cand, lst, x):
+            if x == target:
+                res.append(lst)
+                return
+            if x > target:
+                return
+            for i in range(len(cand)):
+                recurse(cand[i:], lst + [cand[i]], x + cand[i])
+        for i in range(len(candidates)):
+            recurse(candidates[i:], [candidates[i]], candidates[i])
+        return res
