9815637a15c369201cdcdf38ed59937fe5855e79
jsphweid <jsphweid@gmail.com>
2021-11-17 20:41:51 -0600

    multiply-strings

--- lc/answers/multiply-strings/2021.11.17-20.04.55.py
+++ lc/answers/multiply-strings/2021.11.17-20.04.55.py
@@ -1,36 +1,29 @@
 """
-  123
-  456
+Using divmod allows you to write an elegant solution.
+
+  123  -> top
+  456  -> bottom
 x____
-  738
- 6150
-49200
+  738  -> results[0]
+ 6150  -> results[1]
+49200  -> results[2]
 +____
-56088
+56088  -> sum(results)
 
-top=3 bottom=6 carry=1 remainder=8 num=8   i=1 (at end)
-top=2 bottom=6 carry=1 remainder=3 num=38  i=2 (at end)
-top=1 bottom=6 carry=0 remainder=7 num=700 i=3 (at end)
-
-num1=2 num2=3
-carry=0 num=0 i=0 
-top=3 bottom=2 carry=0, remainder=6
-
+Notice how `i` allows us to shift numbers around appropriately.
 """
 
 class Solution:
     def multiply(self, num1: str, num2: str) -> str:
-        if num1 == "0" or num2 == "0": return "0"
         results = []
-        for i, char1 in enumerate(num2[::-1]):
-            carry = 0
-            num = 0
-            for char2 in num1[::-1]:
-                top, bottom = int(char2), int(char1)
+        for i, char_bottom in enumerate(reversed(num2)):
+            num, carry, bottom = 0, 0, int(char_bottom)
+            for char_top in reversed(num1):
+                top = int(char_top)
                 carry, remainder = divmod(top * bottom + carry, 10)
-                num += (remainder * (10 ** i))
+                num += remainder * (10 ** i)
                 i += 1
-            num += (carry * (10 ** i)) if carry else 0
+            num += carry * (10 ** i) if carry else 0
             results.append(num)
         return str(sum(results))
         
