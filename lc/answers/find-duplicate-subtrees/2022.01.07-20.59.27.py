7ce445b030d9e8f4ed977b601fd3e350cd179e39
jsphweid <jsphweid@gmail.com>
2022-01-07 22:53:51 -0600

    find-duplicate-subtrees

--- lc/answers/find-duplicate-subtrees/2022.01.07-20.59.27.py
+++ lc/answers/find-duplicate-subtrees/2022.01.07-20.59.27.py
@@ -1,3 +1,7 @@
+"""
+
+my original solution was a brute force.....
+
 from collections import defaultdict
 
 def serialize_tree(root):
@@ -32,4 +36,38 @@ class Solution:
                 else:
                     s.add(serialized)
         return res
+
+reading discussions now.......
+
+Ya, my approach isn't too far off -- I knew you had to serialize and use set/dict to do lookup
+but my basic fault is that it's extremely expensive for mine to serialize each node. Ideally you
+shouldn't have to traverse all the way down to serialize a different node.
+
+Reading Stefan P's answer made me think of how git hashes contents. You can INCLUDE hashes
+as part of the contents to "include the whole history" (the history itself is used as part of)
+the hash. 
+
+We could do that here if we did DFS.
+
+We get to the end and hash the leaf, then the parent's hash can include
+the leaf hashes. And the grandparent's hash can include the parent's hashes, etc.
+So you don't have to explore all the children over and over to make a new unique hash. You just
+use THEIR hash...
+"""
+
+from collections import defaultdict
+class Solution:
+    def findDuplicateSubtrees(self, root: Optional[TreeNode]) -> List[Optional[TreeNode]]:
+        everything = defaultdict(list)
+
+        def hash_it(node):
+            this_hash = str(hash("None"))
+            if node:
+                this_hash = str(hash(str(node.val) + hash_it(node.left) + hash_it(node.right)))
+                everything[this_hash].append(node)
+            return this_hash
+
+        hash_it(root)
+
+        return [e[0] for e in everything.values() if len(e) > 1]
 
