4e7ab18fc85c8abf974ddd1b20b792f0e49af696
jsphweid <jsphweid@gmail.com>
2022-01-23 15:56:44 -0600

    meeting-rooms-ii

---
+++ lc/answers/meeting-rooms-ii/2022.01.23-15.33.31.py
@@ -0,0 +1,73 @@
+"""
+===== Initial Thoughts =====
+[[0,30],[5,10],[15,20]]
+0                   30
+   5 10  15 20
+
+0 start
+5 start
+10 end
+15 start
+20 end
+30 end
+
+(()())
+
+0-5
+1-2
+4-6
+6-8
+7-9
+7-10
+
+0 start
+1 start
+2 end
+4 start
+5 end
+6 end
+6 start
+7 start
+7 start
+8 end
+9 end
+10 end
+
+(()())((()))
+
+(0,'e')
+
+=== Implemented Approach ===
+seems pretty easy. We'll just change everything to a list of tuples (num, 'start' | 'end')
+then sort by nums
+then do a stack pushing an item on if it is 'start', and popping an item off if it is 'end'.
+The max length of the stack is the answer.
+
+May not matter but ends should come first to keep the stack the smallest... and 'end' < 'start' so sort
+will be good.
+
+~~Complexity Analysis
+Time - O(n + nlogn)
+Space - O(n)
+
+tracing
+[[0,30],[5,10],[15,20]]
+0, 'start'
+5, 'start'
+10, 'end'
+15, 'start'
+20, 'end'
+30, 'end'
+"""
+
+class Solution:
+    def minMeetingRooms(self, intervals: List[List[int]]) -> int:
+        lst, curr, res = [], 0, 0
+        for start, end in intervals:
+            lst.append((start, 'start'))
+            lst.append((end, 'end'))
+        lst.sort()
+        for _, s in lst:
+            curr += 1 if s == "start" else -1
+            res = max(res, curr)
+        return res
