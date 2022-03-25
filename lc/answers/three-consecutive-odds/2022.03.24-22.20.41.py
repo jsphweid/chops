deaf58b5917eac8f6a866920fc424ecec6b5e535
Joseph Weidinger <jsphweid@gmail.com>
2022-03-25 13:52:56 -0500

    three-consecutive-odds

---
+++ lc/answers/three-consecutive-odds/2022.03.24-22.20.41.py
@@ -0,0 +1,7 @@
+class Solution:
+    def threeConsecutiveOdds(self, arr: List[int]) -> bool:
+        if len(arr) < 3: return False
+        for i in range(2, len(arr)):
+            if arr[i] & 1 and arr[i-1] & 1 and arr[i-2] & 1:
+                return True
+        return False
