59e32828b816baa8c781cb5b2477576b402fdc4d
Joseph Weidinger <jsphweid@gmail.com>
2022-03-28 19:48:36 -0500

    car-pooling

---
+++ lc/answers/car-pooling/2022.03.28-19.37.11.py
@@ -0,0 +1,28 @@
+"""
+===== Initial Thoughts =====
+1, add 2
+3, add 3
+5, sub 2
+7, sub 3
+
+~~Complexity Analysis
+Time - O(nlogn + n)
+Space - O(n)
+trips = [[2,1,5],[3,3,7]]
+lst=[(1,2),(1,3),(-1,2),(-1,3)]
+2+3+-2+-3
+"""
+
+class Solution:
+    def carPooling(self, trips: List[List[int]], capacity: int) -> bool:
+        lst = []
+        for num_passengers, _from, _to in trips:
+            lst.append((_to, -1, num_passengers))
+            lst.append((_from, 1, num_passengers))
+        lst.sort()
+        curr = 0
+        for _, inc, num in lst:
+            curr += inc * num
+            if curr > capacity:
+                return False
+        return True
