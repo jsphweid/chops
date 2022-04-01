807f886f15d6cc115094d512b98e3313e9d07201
Joseph Weidinger <jsphweid@gmail.com>
2022-03-31 22:06:08 -0500

    longest-consecutive-sequence

---
+++ lc/answers/longest-consecutive-sequence/2022.03.31-21.39.33.py
@@ -0,0 +1,28 @@
+"""
+===== Initial Thoughts =====
+[0,3,7,2,5,8,4,6,0,1]
+0 1 2   3 4 5   6 7 8
+
+[100,4,200,1,3,2]
+1 2 3 4
+
+100:1, 4;1, 200:1, 1:1, 3:1, 2:1
+
+2 2 2 2 2
+
+I didn't get anything down because I didn't understand that sequence
+meant n, n + 1, n + 2, etc.
+
+saw StefanPochmann's answer, going to replicate that...
+"""
+
+class Solution:
+    def longestConsecutive(self, nums: List[int]) -> int:
+        res, seen = 0, set(nums)
+        for num in nums:
+            if num - 1 not in seen:
+                nxt = num + 1
+                while nxt in seen:
+                    nxt += 1
+                res = max(res, nxt - num)
+        return res
