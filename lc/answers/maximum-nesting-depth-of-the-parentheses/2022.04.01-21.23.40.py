32c63f7142b5cbb1609a6fd16fb578f117f1e8e2
Joseph Weidinger <jsphweid@gmail.com>
2022-04-01 21:28:56 -0500

    maximum-nesting-depth-of-the-parentheses but with O(1) space

--- lc/answers/maximum-nesting-depth-of-the-parentheses/2022.04.01-21.23.40.py
+++ lc/answers/maximum-nesting-depth-of-the-parentheses/2022.04.01-21.23.40.py
@@ -4,17 +4,22 @@ pretty sure we can just ignore anything not ( or )
 and keep a stack whose length is the max depth at any given time
 
 ~~Complexity Analysis
-Time - 
-Space - 
+Time - O(n)
+Space - O(n) (because in string like "(((())))" it can be half of N, which is N)
+
+Actually we don't need to keep the stack... we can just keep a number...
+
+Time - O(n)
+Space - O(1)
 """
 
 class Solution:
     def maxDepth(self, s: str) -> int:
-        stack, res = [], 0
+        curr = res = 0
         for char in s:
             if char == "(":
-                stack.append("(")
-                res = max(res, len(stack))
+                curr += 1
+                res = max(res, curr)
             elif char == ")":
-                stack.pop()
+                curr -= 1
         return res
