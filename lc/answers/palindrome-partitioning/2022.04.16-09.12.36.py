49f3993022955db6c0985f4d4a9920154c5cd608
Joseph Weidinger <jsphweid@gmail.com>
2022-04-16 09:34:46 -0500

    palindrome-partitioning

---
+++ lc/answers/palindrome-partitioning/2022.04.16-09.12.36.py
@@ -0,0 +1,59 @@
+"""
+===== Initial Thoughts =====
+aab, each char by itself is a palindrome
+aa b
+
+abbababa
+abba b a b a
+
+["a","b","b","a","b","a","b","a"],
+["a","b","b","a","b","aba"],
+["a","b","b","a","bab","a"],
+["a","b","b","aba","b","a"],
+["a","b","b","ababa"],
+["a","b","bab","a","b","a"],
+["a","b","bab","aba"],
+["a","b","babab","a"],
+["a","bb","a","b","a","b","a"],
+["a","bb","a","b","aba"],
+["a","bb","a","bab","a"],
+["a","bb","aba","b","a"],
+["a","bb","ababa"],
+["abba","b","a","b","a"],
+["abba","b","aba"],
+["abba","bab","a"]]
+
+We should be able to solve this recursively.
+a + b + others
+a + bb + others
+
+aab
+recurse("aab", [])
+    recurse("ab", ["a"])
+        recurse("b", ["a", "a"])
+            recurse("", ["a", "a", "b"])
+    recurse("b", ["aa"])
+        recurse("", ["aa", "b"])
+"""
+
+class Solution:
+    def partition(self, s: str) -> List[List[str]]:
+        if not s: return []
+        self.res = []
+        self.recurse(s, [])
+        return self.res
+
+    def recurse(self, s: str, path):
+        if not s:
+            self.res.append(path)
+        for i in range(len(s)):
+            section = s[:i+1]
+            if self.is_palindrome(section):
+                self.recurse(s[i+1:], path + [section])
+
+    def is_palindrome(self, s: str):
+        N = len(s)
+        for i in range(N // 2):
+            if s[i] != s[N - i - 1]:
+                return False
+        return True
