76f59f061b38543f165029c83f7d2414b2de500b
Joseph Weidinger <jsphweid@gmail.com>
2022-06-04 09:45:54 -0500

    check-if-all-characters-have-equal-number-of-occurrences

---
+++ lc/answers/check-if-all-characters-have-equal-number-of-occurrences/2022.06.04-09.42.38.py
@@ -0,0 +1,9 @@
+"""
+~~Complexity Analysis
+Time - O(n)
+Space - O(n)
+"""
+from collections import Counter
+class Solution:
+    def areOccurrencesEqual(self, s: str) -> bool:
+        return len(set(Counter(s).values())) == 1
