b1ae2c0e7784089a4887ef69a2879a0c8e9060ef
Joseph Weidinger <jsphweid@gmail.com>
2022-06-05 16:40:07 -0500

    count-collisions-on-a-road

--- lc/answers/count-collisions-on-a-road/2022.06.05-16.04.34.py
+++ lc/answers/count-collisions-on-a-road/2022.06.05-16.04.34.py
@@ -45,6 +45,25 @@ But why won't this work???
 
 SRRLRLRSRLRSSRRLSLRLLRSLSLLSSRRLSRSLSLRRS
 because it's not counting the two R's on the right. bool(total) isn't good enough.
+
+class Solution:
+    def countCollisions(self, directions: str) -> int:
+        total = acc = 0
+        stack = list(directions)
+        right_blocked = False
+        while stack:
+            curr = stack.pop()
+            if curr == "L" or curr == "S":
+                right_blocked = True
+            if curr == "L":
+                acc += 1
+            if curr == "R" or curr == "S":
+                total += acc
+                acc = 0
+            if curr == "R":
+                total += right_blocked
+        return total
+
 """
 
 class Solution:
